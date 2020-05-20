6fc0daafbc67fbb22badb98b393329b55a09f58c
togoc <309128090@qq.com>
2020-05-20 15:26:42 +0800

    加粗首页显示

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -106,16 +106,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad() {
     this.getListFoo = this.createGetListFoo()
     this.getList()
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
   },
 
   /**
@@ -128,21 +121,7 @@ Page({
         active: 1
       })
     }
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    wx.pageScrollTo({ selector: "#hd" })
   },
 
   /**
@@ -153,17 +132,4 @@ Page({
     wx.stopPullDownRefresh()
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
